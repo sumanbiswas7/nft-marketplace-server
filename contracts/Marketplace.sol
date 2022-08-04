23d608d360271ef10d9d5525243e3ce57559fa3a
suman <ronaldosuman97@gmail.com>
2022-08-04 23:39:07 +0530

    purchase function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+// Errors
 error Marketplace__PriceLow();
+error Marketplace__AlreadySold();
+error Marketplace__InvalidItem(uint256 itemId);
 
 contract Marketplace is ReentrancyGuard {
     struct Item {
@@ -31,6 +34,14 @@ contract Marketplace is ReentrancyGuard {
         uint256 tokenId,
         address indexed seller
     );
+    event Bought(
+        uint256 itemId,
+        address indexed nft,
+        address indexed buyer,
+        address indexed seller,
+        uint256 tokenId,
+        uint256 price
+    );
 
     constructor(uint256 _feePercent) {
         i_feePercent = _feePercent;
@@ -56,5 +67,35 @@ contract Marketplace is ReentrancyGuard {
         );
 
         emit Offered(itemCount, address(_nft), _price, _tokenId, msg.sender);
+    }
+
+    function purchaseItem(uint256 _itemId) external payable nonReentrant {
+        uint256 totalPrice = getTotalPrice(_itemId);
+        Item memory item = s_items[_itemId];
+
+        if (msg.value < totalPrice) revert Marketplace__PriceLow(); // paying less than item-price
+        if (item.sold == true) revert Marketplace__AlreadySold(); // item already sold
+        if (_itemId <= 0 || _itemId > itemCount)
+            revert Marketplace__InvalidItem(_itemId); // given itemId to this func id invalid
+
+        item.seller.transfer(item.price);
+        i_feeAccount.transfer(totalPrice - item.price);
+
+        item.sold = true;
+
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            msg.sender,
+            item.seller,
+            item.tokenId,
+            item.price
+        );
+    }
+
+    // View & pure functions
+    function getTotalPrice(uint256 _itemId) public view returns (uint256) {
+        return ((s_items[_itemId].price * (100 + i_feePercent)) / 100);
     }
 }
